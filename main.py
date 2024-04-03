c752cfaca7c290d8afda946de28aa1991633bd0b
KrzysztofiniOWO <91846431+KrzysztofiniOWO@users.noreply.github.com>
2024-04-03 23:20:41 +0200

    You can scrap as many cars as you like

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
 import scrapping_functions as scr
-import dictionaries_fucntions as dic
-import database_functions as dat
 
-data_list = scr.scrap_data()
-filtered_data_list = dic.filter_data(data_list)
-
-for data in filtered_data_list:
-    for key, value in data.items():
-        print(f"{key}: {value}")
-    print()
-
-dat.save_to_mongodb(filtered_data_list)
+scr.scrap_data(3)
--- scrapping_functions.py
+++ scrapping_functions.py
@@ -4,74 +4,41 @@ from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 from bs4 import BeautifulSoup
 
-# def scrap_data():
-#     """This function scraps data from otomoto.pl and prints it to the console"""
-#     service = Service(GeckoDriverManager().install())
-#     options = webdriver.FirefoxOptions()
-#     options.add_argument("--headless")
-#     driver = webdriver.Firefox(service=service, options=options)
+from dictionaries_fucntions import filter_data
+from database_functions import save_to_mongodb
 
-#     url = "https://www.otomoto.pl/osobowe"
-#     driver.get(url)
-
-#     soup = BeautifulSoup(driver.page_source, "html.parser")
-#     articles = soup.find_all("article", class_="ooa-yca59n")
-
-#     for article in articles:
-#         link = article.find("a", href=True)
-#         if link:
-#             article_url = link['href']
-#             driver.get(article_url)
-#             article_soup = BeautifulSoup(driver.page_source, "html.parser")
-#             divs = article_soup.find_all("div", {"data-testid": "advert-details-item"})
-#             data_dict = {}
-#             for div in divs:
-#                 pairs = div.find_all(["p", "a"])
-#                 for i in range(0, len(pairs), 2):
-#                     key = pairs[i].get_text(strip=True)
-#                     value = pairs[i+1].get_text(strip=True)
-#                     data_dict[key] = value
-#             print_data(data_dict)
-#             print()
-
-#             # To można potem wyjebać na razie jest na testa żeby za szybko nie zapierniczał
-#             time.sleep(2)
-
-#     driver.quit()
-
-def scrap_data():
-    """This function scraps data from otomoto.pl and returns it as a list of dictionaries"""
+def scrap_data(num_pages):
+    """This function scraps data from otomoto.pl and saves it to MongoDB"""
     service = Service(GeckoDriverManager().install())
     options = webdriver.FirefoxOptions()
     options.add_argument("--headless")
     driver = webdriver.Firefox(service=service, options=options)
 
-    url = "https://www.otomoto.pl/osobowe"
-    driver.get(url)
+    for page_number in range(1, num_pages + 1):
+        print("next page")
+        url = f"https://www.otomoto.pl/osobowe?page={page_number}"
+        driver.get(url)
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+        articles = soup.find_all("article", class_="ooa-yca59n")
 
-    soup = BeautifulSoup(driver.page_source, "html.parser")
-    articles = soup.find_all("article", class_="ooa-yca59n")
-    
-    data_list = []
-
-    for article in articles:
-        link = article.find("a", href=True)
-        if link:
-            article_url = link['href']
-            driver.get(article_url)
-            article_soup = BeautifulSoup(driver.page_source, "html.parser")
-            divs = article_soup.find_all("div", {"data-testid": "advert-details-item"})
-            data_dict = {}
-            for div in divs:
-                pairs = div.find_all(["p", "a"])
-                for i in range(0, len(pairs), 2):
-                    key = pairs[i].get_text(strip=True)
-                    value = pairs[i+1].get_text(strip=True)
-                    data_dict[key] = value
-            data_list.append(data_dict)
-            
-            # For testing purposes, so it doesn't run too fast
-            time.sleep(1)
+        for article in articles:
+            link = article.find("a", href=True)
+            if link:
+                article_url = link['href']
+                driver.get(article_url)
+                article_soup = BeautifulSoup(driver.page_source, "html.parser")
+                divs = article_soup.find_all("div", {"data-testid": "advert-details-item"})
+                data_dict = {}
+                for div in divs:
+                    pairs = div.find_all(["p", "a"])
+                    for i in range(0, len(pairs), 2):
+                        key = pairs[i].get_text(strip=True)
+                        value = pairs[i+1].get_text(strip=True)
+                        data_dict[key] = value
+                
+                filtered_data = filter_data(data_dict)
+                save_to_mongodb(filtered_data)
+                
+                time.sleep(1)
 
     driver.quit()
-    return data_list
